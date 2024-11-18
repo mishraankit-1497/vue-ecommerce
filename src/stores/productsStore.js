dca24919b97a836ce6c625ab710f46e83ca72b77
mishraankit-1497 <ankitkumarmishra14121997@gmail.com>
2024-11-18 12:13:45 +0530

    updated product store

--- src/stores/productsStore.js
+++ src/stores/productsStore.js
@@ -1,72 +1,35 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 import axios from "axios";
+import {
+  createProduct,
+  deleteProduct,
+  fetchProducts,
+  updateProduct,
+} from "@/utils/utils";
 
 export const useProductStore = defineStore("productsStore", {
   // Defining initial state
   state: () => ({
-    categories: [],
     products: [],
   }),
   // Defining actions
   actions: {
     // Defining asynchronous function for API call
-    // Getting list of product categories
-    async fetchCategories() {
-      try {
-        const fetchResponse = await axios.get(
-          "http://localhost:3000/product-categories"
-        );
-        this.categories = fetchResponse.data;
-      } catch (error) {
-        console.log("Failed to fetch categories..", error);
-      }
-    },
-    async fetchProducts() {
+    async getProducts() {
       //GET: Getting product list
       try {
-        const fetchResponse = await axios.get("http://localhost:3000/shop");
+        const fetchResponse = await fetchProducts();
         this.products = fetchResponse.data;
       } catch (error) {
         console.log("Failed to fetch products..", error);
       }
     },
-    // POST: Creating new product category
-    async createCategory(category) {
-      try {
-        const fetchResponse = await axios.post(
-          "http://localhost:3000/product-categories",
-          category
-        );
-        this.categories.push(fetchResponse.data);
-      } catch (error) {
-        console.log("Failed to create category", error);
-      }
-    },
-    // DELETE: Deleting product category
-    async deleteCategory(categoryId) {
-      try {
-        await axios.delete(
-          `http://localhost:3000/product-categories/${categoryId}`
-        );
-        this.categories = this.categories.filter(
-          (category) => category.id !== categoryId
-        );
-      } catch (error) {
-        console.log("Failed to delete category", error);
-      }
-    },
     // POST: Create new product
-    async createProduct(product, category) {
+    async addProduct(product, category) {
       try {
         const categoryProduct = { ...category, ...items[product] };
-        console.log(categoryProduct);
-        console.log(product);
-        console.log(category);
-        const response = await axios.post(
-          `http://localhost:3000/shop`,
-          categoryProduct
-        );
+        const response = await createProduct(categoryProduct);
         this.products.push([
           ...response.data
             .filter((c) => c.id === categoryProduct.id)
@@ -77,14 +40,21 @@ export const useProductStore = defineStore("productsStore", {
     // UPDATE: Updating products
     async updateProduct(product) {
       try {
-        const response = await axios.put(
-          `http://localhost:3000/shop`,
-          product
+        const response = await updateProduct(product);
+        const index = this.products.findIndex(
+          (p) => p.id === product.items[0].id
         );
-        const index = this.products.findIndex((p) => p.id === product.items[0].id);
         if (index !== -1) {
           this.products[index] = response.data;
         }
+      } catch (error) {
+        console.error("Failed to update product", error);
+      }
+    },
+    async removeProduct(productId) {
+      try {
+        const response = await deleteProduct(productId);
+        this.products = this.products.filter((p) => p.id !== productId);
       } catch (error) {
         console.error("Failed to update product", error);
       }
